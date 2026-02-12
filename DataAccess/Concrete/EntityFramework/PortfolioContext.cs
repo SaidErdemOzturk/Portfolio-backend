d9967292ccb025c01d66ec03f1ddde2c51b017cd
saiderdemozturk <saiderdemozturk@gmail.com>
2026-02-12 13:03:47 +0300

    PortfolioContext arranged

--- DataAccess/Concrete/EntityFramework/PortfolioContext.cs
+++ DataAccess/Concrete/EntityFramework/PortfolioContext.cs
@@ -14,14 +14,24 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=said5125_MyPortfolio;Trusted_Connection=true");
+            if (optionsBuilder.IsConfigured) return;
+
+            var cs = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
+
+            if (string.IsNullOrWhiteSpace(cs))
+                throw new InvalidOperationException(
+                    "SQL_CONNECTION_STRING environment variable is not set.");
+
+            optionsBuilder.UseSqlServer(cs);
         }
+        //setx SQL_CONNECTION_STRING "Server=104.247.167.202\\MSSQLSERVER2022;Database=said5125_MyPortfolio;User ID=said5125_MyPortfolio;Password=179324865.aA;TrustServerCertificate=True;"
 
-
+        //optionsBuilder.UseSqlServer("Server=104.247.167.202\\MSSQLSERVER2022;Database=said5125_MyPortfolio;User ID=said5125_MyPortfolio;Password=179324865.aA;TrustServerCertificate=True;");
+        //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=said5125_MyPortfolio;Trusted_Connection=true");
         public DbSet<Project> Projects { get; set; }
         public DbSet<Technology> Technologies { get; set; }
-        public DbSet<Image> Images{ get; set; }
-        public DbSet<Job> Jobs{ get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Job> Jobs { get; set; }
         public DbSet<JobDescription> JobDescriptions { get; set; }
         public DbSet<ProjectTechnology> ProjectTechnologies { get; set; }
         public DbSet<Profile> Profile { get; set; }
